7b0c73d76d4f3294e410e4e259a39b2014d8e5e1
fuadslymnv <fuadsuleymanov03@gmail.com>
2024-02-17 15:23:18 +0400

    Icons added

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import { AntDesign } from "@expo/vector-icons";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -58,16 +59,18 @@ function GameScreen({ userNumber, onGameOver }) {
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
-        <InstructionText style={styles.instructionText}>Higher or lower</InstructionText>
+        <InstructionText style={styles.instructionText}>
+          Higher or lower
+        </InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <AntDesign name="minus" size={22} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
+              <AntDesign name="plus" size={22} color="white" />
             </PrimaryButton>
           </View>
         </View>
@@ -89,7 +92,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   instructionText: {
-    marginBottom:12
-  }
+    marginBottom: 12,
+  },
 });
 export default GameScreen;
